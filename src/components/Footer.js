040b563ecaf565f61fd2e4b880156c72a180e0c3
clue4 <72981805+clue4@users.noreply.github.com>
2021-08-21 22:40:51 -0400

    some minor changes, hopefully time for some larger add-ons soon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,10 @@ function Footer() {
   return (
     <div className='footer'>
       <div className='footer-container'>
-        <p className='footer-item'>Created by Connie Liu   
-        <a href="https://www.youtube.com/watch?v=I2UBjN5ER4s"
-          className='footer-item'>Inspiration from Brian Design</a></p>
+        <ul className='footer-items'>
+          <li>Created by Connie Liu</li>
+          <li><a href="https://www.youtube.com/watch?v=I2UBjN5ER4s">Inspiration from Brian Design</a></li>
+        </ul>
       </div>
     </div>
   )
