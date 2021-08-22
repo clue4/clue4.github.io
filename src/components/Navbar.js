8654bc9be8cad461364e2eb21777746dd3698294
clue4 <72981805+clue4@users.noreply.github.com>
2021-08-22 18:30:28 -0400

    responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,24 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaRegWindowClose } from "react-icons/fa";
 import './Navbar.css';
+
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  
-  
-  // const handleClick = () => setClick(!click);
+  const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
   return (
-    <>
     <nav className="navbar">
       <div className="navbar-container"> 
         <Link to="/" className="navbar-logo">
           Connie Liu
         </Link>
-        {/* <div className='menu-icon' onClick={handleClick}>
-          <i className={click ? 'fas fa-times' : 'fas fa-bars'} /> 
-        </div>  */}
+        <div className='menu-icon' onClick={handleClick}>
+          {click ? ( <FaRegWindowClose className="mobile-icon" />) : 
+          ( <FaBars className="mobile-icon" /> )}
+        </div> 
         <ul className={click ? 'nav-menu active' : 'nav-menu'}>
           <li className='nav-item'>
             <Link to='/' className="nav-links" onClick={closeMobileMenu}>
@@ -33,7 +33,6 @@ function Navbar() {
         </ul>
       </div>
     </nav>
-    </>
   );
 };
 
